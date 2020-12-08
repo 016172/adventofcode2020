311efe2f1a4f4621efdf92c12f9e90a8f4ed28c1
016172 <seolmi@hotmail.se>
2020-12-09 00:03:02 +0100

    day8 completed

--- day8-2.py
+++ day8-2.py
@@ -1,62 +1,46 @@
 lin = []
-acc = 0
-switch = False
 with open('input.txt', 'r') as file:
     lines = file.readlines()
 for line in lines:
     if line != "\n":
         lin.append(line.replace("\n",  ""))
-executed = [False]*len(lin)
-i = 0
-while i < len(lin):
-    st = lin[i].split(" ")
-    instr, v = st[0],int(st[1])
-    print(instr,v)
-    if not executed[i]:
-        if instr == "acc":
-            executed[i] = True
-            i += 1
-            acc += int(v)
-        elif instr == "jmp":
-            executed[i] = True
-            if "jmp" in [i[:3] for i in lin[i+int(v):i-1]]:
-                if not switch:
-                    lin[i] = "nop " + str(v)
-                    switch = True
-                    i += 1
-                else:
-                    i += int(v)
-        elif instr == "nop":
-            print("här1")
-            executed[i] = True
-
-            if not switch:
-                print("wooo")
-                after = [i[:3] for i in lin[i+1:]]
-                print(after)
-                if "jmp" in after:
-                    print("här2")
-                    nv = after.index("jmp")
-                    print(str(lin[nv+1]) + " wooo23123")
-                    if executed[int(lin[nv+1].split(" ")[1])+i]:
-                        lin[i] = "jmp " + str(2)
-                        i += 2
-                        switch = True
-                    else:
-                        print("här3")
-                        i += 1
-                        print(i)
+def ite():
+    executed = [False]*len(lin)
+    acc = 0
+    i = 0
+    while i < len(lin):
+        h = lin[i].split(" ")
+        instr,v = h[0],h[1]
+        if not executed[i]:
+            if instr == "acc":
+                executed[i] = True
+                i += 1
+                acc += int(v)
+            elif instr == "jmp":
+                executed[i] = True
+                i = i + int(v)
             else:
-                print("wtf")
-                i += int(v)
+                executed[i] = True
+                i += 1
         else:
-            print("här")
-            executed[i] = True
-            i += 1
-    else:
-        break
-print(acc)
-
-
-
-#print(lin)
+            return None
+    return acc
+for j in range(0,len(lin)):
+    st = lin[j].split(" ")
+    instr, v = st[0],st[1]
+    if instr == "nop":
+        lin[j] = "jmp " + v
+        test = ite()
+        if test == None:
+            lin[j] = instr + " " + v
+        else:
+            print(test)
+            break
+    elif instr == "jmp":
+        lin[j] = "nop " + str(v)
+        test = ite()
+        if test == None:
+            lin[j] = instr + " " + v
+        else:
+            print(test)
+            break
