719511ce8648494c206b17a6d864d1df2d2beb75
Andreas Olsson <andreasolsson@MacBook-Pro-som-tillhor-Andreas.local>
2020-12-11 23:55:19 +0100

    day 11, part 1, shorter

--- day11-1.py
+++ day11-1.py
@@ -9,88 +9,61 @@ def findadj(i,j):
     nbr = 0
     if not ins[i][j] == ".":
         if i == 0 and (not j == 0 and not j+1 == length):
-            if ins[i][j-1] == "#":
-                nbr += 1
-            if ins[i][j+1] == "#":
-                nbr += 1
-            if ins[i+1][j-1] == "#":
-                nbr += 1
+            for x in [i,i+1]:
+                for y in [j-1,j+1]:
+                    if ins[x][y] == "#":
+                        nbr += 1
             if ins[i+1][j] == "#":
-                nbr += 1
-            if ins[i+1][j+1] == "#":
                 nbr += 1
         elif 1 <= i <= height-2 and j == 0:
-            if ins[i-1][j] == "#":
-                nbr += 1
-            if ins[i-1][j+1] == "#":
-                nbr += 1
+            for x in [i-1,i+1]:
+                for y in [j,j+1]:
+                    if ins[x][y] == "#":
+                        nbr += 1
             if ins[i][j+1] == "#":
-                nbr += 1
-            if ins[i+1][j+1] == "#":
-                nbr += 1
-            if ins[i+1][j] == "#":
                 nbr += 1
         elif i+1 == height and (not j == 0 and not j+1 == length):
-            if ins[i][j-1] == "#":
-                nbr += 1
-            if ins[i-1][j-1] == "#":
-                nbr += 1
+            for x in [i-1,i]:
+                for y in [j-1,j+1]:
+                    if ins[x][y] == "#":
+                        nbr += 1
             if ins[i-1][j] == "#":
-                nbr += 1
-            if ins[i-1][j+1] == "#":
-                nbr += 1
-            if ins[i][j+1] == "#":
                 nbr += 1
         elif j+1 == length and 1 <= i <= height-2:
-            if ins[i+1][j] == "#":
-                nbr += 1
-            if ins[i+1][j-1] == "#":
-                nbr += 1
+            for x in [i-1,i+1]:
+                for y in [j-1,j]:
+                    if ins[x][y] == "#":
+                        nbr += 1
             if ins[i][j-1] == "#":
-                nbr += 1
-            if ins[i-1][j-1] == "#":
-                nbr += 1
-            if ins[i-1][j] == "#":
                 nbr += 1
         elif 1 <= i <= height-2 and 1 <= j <= length-2:
+            for x in [i-1,i+1]:
+                for y in [j-1,j+1]:
+                    if ins[x][y] == "#":
+                        nbr += 1
             if ins[i][j-1] == "#":
                 nbr += 1
-            if ins[i-1][j-1] == "#":
-                nbr += 1
             if ins[i-1][j] == "#":
-                nbr += 1
-            if ins[i-1][j+1] == "#":
                 nbr += 1
             if ins[i][j+1] == "#":
                 nbr += 1
-            if ins[i+1][j+1] == "#":
-                nbr += 1
             if ins[i+1][j] == "#":
                 nbr += 1
-            if ins[i+1][j-1] == "#":
-                nbr += 1
-
     return nbr
-t = 300
 for i in range(height):
     tmp = []
     for j in range(length):
         if ins[i][j] == "L":
             tmp.append("#")
         else:
-            if ins[i][j] == "#":
-                tmp.append("#")
-            else:
-                tmp.append(".")
+            tmp.append(ins[i][j])
     ins[i] = tmp
 while True:
-    a = 0
-    gl = 0
+    a, gl,adj = 0,0, []
     for i in range(height):
         for j in range(length):
             if ins[i][j] == "#":
                 a += 1
-    adj = []
     for i in range(height):
         new = []
         for j in range(length):
