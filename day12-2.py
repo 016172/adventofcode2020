faa73c5d9e042d3d827f2d5094b8e0ee8512feee
Andreas Olsson <andreasolsson@MacBook-Pro-som-tillhor-Andreas.local>
2020-12-12 19:58:01 +0100

    day12, part 2 solved

---
+++ day12-2.py
@@ -0,0 +1,46 @@
+ins = []
+dirs = {"N":0,"E":0,"S":0,"W":0,"dir":"E"}
+deg = [0,90,180,270]
+totdegree = 0
+waypoint = [10,0,0,1]
+di = ["E","S","W","N"]
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+for line in lines:
+    if line != "\n":
+        ins.append(line.replace("\n",  ""))
+for i in ins:
+    instr,v = i[:1],int(i[1:])
+    if instr == "F":
+        newPos = [i*v for i in waypoint]
+        for i in range(len(newPos)):
+            dirs[di[i]] += newPos[i]
+    elif instr == "R":
+        totdegree = (totdegree + v)%360
+        dirs["dir"] = di[deg.index(totdegree)]
+        if v == 90:
+            waypoint = [waypoint[3],waypoint[0],waypoint[1],waypoint[2]]
+        elif v == 180:
+            waypoint = [waypoint[2],waypoint[3],waypoint[0],waypoint[1]]
+        elif v == 270:
+            waypoint = [waypoint[1],waypoint[2],waypoint[3],waypoint[0]]
+    elif instr == "L":
+        totdegree = (totdegree - v)%360
+        dirs["dir"] = di[deg.index(totdegree)]
+        if v == 90:
+            waypoint = [waypoint[1],waypoint[2],waypoint[3],waypoint[0]]
+        elif v == 180:
+            waypoint = [waypoint[2],waypoint[3],waypoint[0],waypoint[1]]
+        elif v == 270:
+            waypoint = [waypoint[3],waypoint[0],waypoint[1],waypoint[2]]
+    else:
+        waypoint[di.index(instr)] += v
+print(abs(dirs["N"]-dirs["S"]) + abs(dirs["E"]-dirs["W"]))
+
+
+
+
+
+    #1
+#2     #3
+    #4
