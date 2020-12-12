b1a59f31f1853bcf2535250d6e2a85961bc9fba6
016172 <seolmi@hotmail.se>
2020-12-12 01:33:58 +0100

    better solution

--- day11-1.py
+++ day11-1.py
@@ -5,82 +5,50 @@ for line in lines:
     if line != "\n":
         ins.append(line.replace("\n",  ""))
 height,length = len(ins),len(ins[0])
-def findadj(i,j):
-    nbr = 0
-    if not ins[i][j] == ".":
-        if i == 0 and (not j == 0 and not j+1 == length):
-            for x in [i,i+1]:
-                for y in [j-1,j+1]:
-                    if ins[x][y] == "#":
-                        nbr += 1
-            if ins[i+1][j] == "#":
-                nbr += 1
-        elif 1 <= i <= height-2 and j == 0:
-            for x in [i-1,i+1]:
-                for y in [j,j+1]:
-                    if ins[x][y] == "#":
-                        nbr += 1
-            if ins[i][j+1] == "#":
-                nbr += 1
-        elif i+1 == height and (not j == 0 and not j+1 == length):
-            for x in [i-1,i]:
-                for y in [j-1,j+1]:
-                    if ins[x][y] == "#":
-                        nbr += 1
-            if ins[i-1][j] == "#":
-                nbr += 1
-        elif j+1 == length and 1 <= i <= height-2:
-            for x in [i-1,i+1]:
-                for y in [j-1,j]:
-                    if ins[x][y] == "#":
-                        nbr += 1
-            if ins[i][j-1] == "#":
-                nbr += 1
-        elif 1 <= i <= height-2 and 1 <= j <= length-2:
-            for x in [i-1,i+1]:
-                for y in [j-1,j+1]:
-                    if ins[x][y] == "#":
-                        nbr += 1
-            if ins[i][j-1] == "#":
-                nbr += 1
-            if ins[i-1][j] == "#":
-                nbr += 1
-            if ins[i][j+1] == "#":
-                nbr += 1
-            if ins[i+1][j] == "#":
-                nbr += 1
-    return nbr
 for i in range(height):
-    tmp = []
+    tmp = ['0']
     for j in range(length):
         if ins[i][j] == "L":
             tmp.append("#")
         else:
             tmp.append(ins[i][j])
-    ins[i] = tmp
+    ins[i] = tmp + ['0']
+ins.insert(0,['0']*(length+2))
+ins.insert(len(ins),['0']*(2+length))
+def findadj(i,j):
+    nbr = 0
+    if not ins[i][j] == ".":
+        for x in range(i-1,i+2,1):
+            for y in range(j-1,j+2,1):
+                if (x,y) != (i,j):
+                    if ins[x][y] == "#":
+                        nbr += 1
+    return nbr
 while True:
     a, gl,adj = 0,0, []
-    for i in range(height):
-        for j in range(length):
+    for i in range(1,height+1):
+        for j in range(1,length+1):
             if ins[i][j] == "#":
                 a += 1
-    for i in range(height):
+    for i in range(1,height+1):
         new = []
-        for j in range(length):
+        for j in range(1,length+1):
             new.append(findadj(i,j))
-        adj.append(new)
-    for i in range(height):
+        adj.append([0] + new + [0])
+    adj.insert(0,[0]*(length+2))
+    adj.insert(len(adj),[0]*(length+2))
+    for i in range(1,height+1):
         tmp = []
-        for j in range(length):
+        for j in range(1,length+1):
             if adj[i][j] >= 4 and ins[i][j] == "#":
                 tmp.append("L")
             elif adj[i][j] == 0 and ins[i][j] == "L":
                 tmp.append("#")
             else:
                 tmp.append(ins[i][j])
-        ins[i] = tmp
-    for i in range(height):
-        for j in range(length):
+        ins[i] = [0] + tmp + [0]
+    for i in range(1,height+1):
+        for j in range(1,length+1):
             if ins[i][j] == "#":
                 gl += 1
     if gl == a:
