6c5fe87707e7c69394805180c42f25b07520f4d4
016172 <seolmi@hotmail.se>
2020-12-14 00:41:46 +0100

    day13,part2,solved

--- day13-2.py
+++ day13-2.py
@@ -6,17 +6,27 @@ for line in lines:
         ins.append(line.replace("\n",  ""))
 busses = ins[0].split(',')
 offset = []
+def euclides_extended_algorithm(z,n):
+    if z == 0:
+        return 0,1
+    z1,n1 = euclides_extended_algorithm(n%z,z)
+    t1 = n1 - (n//z) * z1
+    t2 = z1
+    return t1,t2
 for i in range(len(busses)):
     offset.append((busses[i],i))
 offset = [(int(_[0]),_[1]) for _ in offset if _[0] != 'x']
-t = 0
-modcalc = offset[0][0]
-print(offset)
-while True:
-    if all((t+y)%modcalc==y for (x,y) in offset):
-        break
-    else:
-        t += 1
-        print(t)
-
-print(t)
+M = 1
+a,bprime,b,m = [],[],[],[]
+for i in offset:
+    M = M * i[0]
+    a.append(-i[1]%i[0])
+    m.append(i[0])
+for i in offset:
+    b.append(M//i[0])
+for i in range(len(offset)):
+    bprime.append(euclides_extended_algorithm(b[i],m[i])[0]%m[i])
+sum = 0
+for z in range(len(bprime)):
+    sum += a[z]*b[z]*bprime[z]
+print(sum%M)
