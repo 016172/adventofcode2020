6f8bbfb156f1fd826f43a2d684ddf04cf8d7392c
016172 <seolmi@hotmail.se>
2020-12-15 21:58:41 +0100

    day14,part2 solved, however, very slow solution?

--- day14-2.py
+++ day14-2.py
@@ -1,5 +1,5 @@
 from itertools import combinations
-ins,vmask,stored = [],0,dict()
+ins,vmask,stored,sum = [],0,dict(),0
 with open('input.txt', 'r') as file:
     lines = file.readlines()
 for line in lines:
@@ -13,19 +13,14 @@ for i in range(len(ins)):
         tried = []
         sum = 0
         loc,val = int(z[0].split("[")[1].replace("]","")),list(bin(int(z[1])).split('b')[1])
-        for i in range(len(vmask)-len(val),0,-1):
-            val.insert(0,'0')
-        for i in range(len(vmask)):
-            if val[i] != vmask[i] and vmask[i] != 'X':
-                val[i] = vmask[i]
         locs = list(bin(loc).split('b')[1])
-        print(''.join(locs))
         #blir X1001X istället för X1101X
         for i in range(len(vmask)-len(locs),0,-1):
             locs.insert(0,'0')
         for i in range(len(vmask)):
-            locs[i] = vmask[i]
-        print(''.join(str(e) for e in locs))
+            if vmask[i] != '0':
+                locs[i] = vmask[i]
+        #print(''.join(str(e) for e in locs))
         for k in combinations([0,1]*locs.count('X'),locs.count('X')):
             if list(k) not in tried:
                 tried.append(list(k))
@@ -33,21 +28,13 @@ for i in range(len(ins)):
         for i in range(len(locs)):
             if locs[i] == 'X':
                 indices.append(i)
-#        print(indices)
         for i in tried:
             cntr = 0
             tmp = locs
             for j in indices:
                 tmp[j] = i[cntr]
                 cntr += 1
-    #        print(''.join(str(e) for e in tmp))
-
-
-
-
-
-
-
-
-
-print(stored)
+            stored[int(''.join(str(e) for e in tmp),2)] = ''.join(val)
+for i in stored.values():
+    sum += int(i,2)
+print(sum)
